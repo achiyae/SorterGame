5b9b683eb4f5c388b4e2bc3dca69b2f727183861
Achiya Elyasaf <10044875+achiyae@users.noreply.github.com>
2023-05-31 23:32:58 +0300

    finish

--- webpage/script.js
+++ webpage/script.js
@@ -1,26 +1,122 @@
 const DURATION = 500
+
 function updateRobotLocation() {
     $('#robot-value-x').text(Math.round($('#robot').offset().left))
     $('#robot-value-y').text(Math.round($('#robot').offset().top))
 }
 
 function updateRobotEngine(h, v) {
-    if(!h) h = 0
-    if(!v) v = 0
+    if (!h) h = 0
+    if (!v) v = 0
     $('#robot-value-motor-h').text(h)
     $('#robot-value-motor-v').text(v)
 }
 
 function updateRobotMagnet(v) {
-    if(!v) v = false
-    if(!v){
+    if (!v) v = false
+    if (!v) {
         $('#robot-value-magnet').text('Off').css("background-color", 'transparent')
     } else {
         $('#robot-value-magnet').text('On').css("background-color", '#51EF51FF')
     }
 }
 
+function animateKeyframe(isRelative, left, top) {
+    if (left != null) {
+        if (isRelative) {
+            left = "+=" + left + "px"
+        } else {
+            left = left + "px"
+        }
+        return {left: left,}
+    } else if (top != null) {
+        if (isRelative) {
+            top = "+=" + top + "px"
+        } else {
+            top = top + "px"
+        }
+        return {top: top,}
+    }
+}
+
+function animateOptions(left, top, options) {
+    if (!options) options = {}
+    let ans = {
+        duration: options.duration || DURATION,
+        step: options.step || function (now, tween) {
+            updateRobotLocation()
+        },
+        start: options.start || function () {
+            if (left != null)
+                updateRobotEngine(left < 0 ? -10 : 10, 0)
+            else if (top != null)
+                updateRobotEngine(0, top < 0 ? -10 : 10)
+        },
+        complete: options.complete || function () {
+            updateRobotEngine(0, 0)
+        },
+        progress: options.progress || null,
+    }
+
+    return ans
+}
+
+
+function findColor(color) {
+    let queue = 'findColor-' + Math.random()
+    let bowel = $('.bowel')
+    let robot = $('#robot')
+    let firstScanLocation = {
+        left: bowel.offset().left - robot.offset().left + 1,
+        top: bowel.height() - 7
+    }
+    robot
+        .animate(animateKeyframe(true, firstScanLocation.left, null), animateOptions(firstScanLocation.left, null, {queue: queue}))
+        .animate(animateKeyframe(true, null, firstScanLocation.top), animateOptions(null, firstScanLocation.top, {queue: queue}))
+        .animate(animateKeyframe(true, bowel.width() - 11, null),
+            animateOptions(bowel.width() - 11, null, {
+                queue: queue,
+                duration: DURATION * 5,
+                step: function (now, tween) {
+                    if (Math.floor($('.block.' + color + ':first').offset().left) === Math.floor(robot.offset().left + 2)) {
+                        console.log('found')
+                        robot.stop(true)
+                    }
+                    updateRobotLocation()
+                },
+                complete: function () {
+                    // pickUp(color)
+                }
+            }))
+}
+
+function pickUp(color) {
+    updateRobotMagnet(true)
+    $('.block.' + color + ':first').className = 'block transparent'
+    $('#robot-block').className = 'block ' + color
+    goToPile(color)
+}
+
+function putDown() {
+    updateRobotMagnet(false)
+}
+
+function goToPile(color) {
+    let pile = $('.pile.' + color + '-b:first')
+    let robot = $('#robot')
+    let bowel = $('.bowel')
+    let counter = $('#' + color + '-counter')
+    let lastTransparentBlock = pile.find('.pile-block.transparent:first')
+    console.log('here')
+    robot
+        .animate(animateKeyframe(true, null, -bowel.height() + 7), animateOptions(null, -bowel.height() + 7, {}))
+}
+
 function move(color) {
+    findColor(color)
+}
+
+function move2(color) {
     var robot = document.getElementById('robot')
     var robotBlock = document.getElementById('robot-block')
     var blocks = document.getElementsByClassName('block ' + color)
@@ -148,7 +244,7 @@ function move(color) {
     }
 }
 
-$( document ).ready(function() {
+$(document).ready(function () {
     updateRobotLocation()
     updateRobotEngine()
     updateRobotMagnet()
