92503bb2ddf5a5bbef6c2261e0f424d4fd3e4dd1
Achiya Elyasaf <10044875+achiyae@users.noreply.github.com>
2023-06-13 23:57:45 +0300

    update

---
+++ test/spec/non-js/events-empty.js
@@ -0,0 +1,38 @@
+// @provengo summon selenium
+// @provengo summon ctrl
+
+let press;
+defineEvent(SeleniumSession, "Press", function (session, event) {
+})
+
+let magnetFailure;
+defineEvent(SeleniumSession, "MagnetFailure", function (session, event) {
+})
+
+let verifyCounter;
+defineEvent(SeleniumSession, "VerifyCounter", function (session, event) {
+})
+
+let waitForCounter;
+defineEvent(SeleniumSession, "WaitForCounter", function (session, event) {
+})
+
+let enableSensorsMalfunction;
+defineEvent(SeleniumSession, "EnableSensorsMalfunction", function (session, event) {
+})
+
+let chooseAlwaysGreenMalfunction;
+defineEvent(SeleniumSession, "ChooseAlwaysGreenMalfunction", function (session, event) {
+})
+
+let chooseNoiseMalfunction;
+defineEvent(SeleniumSession, "ChooseNoiseMalfunction", function (session, event) {
+})
+
+let dropMagnetMalfunction;
+defineEvent(SeleniumSession, "DropMagnetMalfunction", function (session, event) {
+})
+
+let waitForMagnetOn;
+defineEvent(SeleniumSession, "WaitForMagnetOn", function (session, event) {
+})
