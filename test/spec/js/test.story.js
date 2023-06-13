408c7e3c3899648955da4dff39a2a54b3492e9e5
Achiya Elyasaf <10044875+achiyae@users.noreply.github.com>
2023-06-13 23:57:05 +0300

    update

--- test/spec/js/test.story.js
+++ test/spec/js/test.story.js
@@ -51,36 +51,31 @@ for (let i = 0; i < colors.length; i++) {
 
 //region Test 3: Add constraint - after pressing, block another press until counter is incremented
 //************************************************************************************************
-/*for (let i = 0; i < colors.length; i++) {
-    let color = colors[i]
-    Constraints
-        .after(any(/Press/).and(any({color: color})))
-        .block(any(/Press/))
-        .until(
-            any(/EndOfAction/).and(any({eventName: "WaitForCounter", color: color}))
-        );
-}*/
+/*bthread('no two consecutive presses', function () {
+    while (true) {
+        waitFor(any(/Press/))
+        sync({
+            block: any(/Press/),
+            waitFor: any({eventName: "WaitForCounter", count: 1}),
+        })
+    }
+})*/
 //endregion
 
-//region Test 4: Add malfunction - sensor 1 is always green
+//region Test 4: Add sensors/actuators malfunctions
 //*********************************************************
 /*bthread('Enable Always-green malfunction', function () {
     with (session.start(URL)) {
         enableSensorsMalfunction()
         chooseAlwaysGreenMalfunction()
     }
-})*/
-//endregion
-
-//region Test 5: Add malfunction - Sensors are noisy
-//*************************************************
-/*bthread('Enable Noise malfunction', function () {
+})
+bthread('Enable Noise malfunction', function () {
     with (session.start(URL)) {
         enableSensorsMalfunction()
         chooseNoiseMalfunction()
     }
 })
-
 bthread('Drop Magnet malfunction', function () {
     with (session.start(URL)) {
         dropMagnetMalfunction()
@@ -88,33 +83,26 @@ bthread('Drop Magnet malfunction', function () {
 })*/
 //endregion
 
-//region Test 6: Add goal for test-suite generation: Reach 3 drop down
-//********************************************************************
+//region Test 5: Add goals for test-suite generation
+//**************************************************
 /*bthread('wait for 3 drop down', function () {
     waitFor(any(/WaitForCounter/).and(any({count: 1})))
     waitFor(any(/WaitForCounter/).and(any({count: 1})))
     waitFor(any(/WaitForCounter/).and(any({count: 1})))
     Ctrl.doMark('Reached 3 drop down')
+})
+bthread('Drop magnet after picking up', function () {
+    waitFor(any(/Press/))
+    inParallel(
+        function() {
+            session.waitForMagnetOn()
+        },
+        function () {
+            let e = waitFor(any(/DropMagnetMalfunction/)
+                .or(any(/EndOfAction/).and(any({eventName: "WaitForCounter", count: 1}))))
+            if (e.name == 'DropMagnetMalfunction') {
+                Ctrl.doMark('magnet-failed-path')
+            }
+        })
 })*/
-//endregion
-
-//region Test 7: Add goal for test-suite generation: Drop magnet after picking up
-//********************************************************************************
-/*for (let i = 0; i < colors.length; i++) {
-    let color = colors[i]
-    bthread('magnet failed after picking up', function () {
-        waitFor(any(/Press/))
-        inParallel(
-            function() {
-                session.waitForMagnetOn()
-            },
-            function () {
-                let e = waitFor(any(/DropMagnetMalfunction/)
-                    .or(any(/EndOfAction/).and(any({eventName: "WaitForCounter", color: color}))))
-                if (e.name == 'DropMagnetMalfunction') {
-                    Ctrl.doMark('magnet-failed-path')
-                }
-            })
-    })
-}*/
 //endregion
